4382615e26a2830667fd71683f5448295a70be76
Vera Sjunnesson <verasjunnesson@mbpsomtlhorvera.mynet>
2023-09-04 14:55:04 +0200

    responsiveness

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { GlobalStyles } from 'components/lib/GlobalStyles'
 import { AllPages } from 'components/pages/AllPages'
-import { ScrollerMotion } from 'scroller-motion'
 
 export const App = () => {
   return (
     <>
       <GlobalStyles />
-      <ScrollerMotion>
-        <AllPages />
-      </ScrollerMotion>
+      <AllPages />
     </>
   )
 }
